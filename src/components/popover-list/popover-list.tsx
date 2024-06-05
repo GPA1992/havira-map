9c6346f21313f1caa7adbebf221cb57edada1b2d
GPA1992 <gabrielproencaaraujo@gmail.com>
2024-06-05 02:13:09 -0300

    feat: icon change

--- src/components/popover-list/popover-list.tsx
+++ src/components/popover-list/popover-list.tsx
@@ -7,14 +7,14 @@ import {
 import { Button } from '@/components/ui/button'
 import { usersColumns } from '@/components/users-table/users-columns'
 import { UsersTable } from '@/components/users-table/users-table'
-import { PersonIcon } from '@radix-ui/react-icons'
+import { LayersIcon } from '@radix-ui/react-icons'
 
 export default function PopoverList() {
   return (
     <Popover>
       <PopoverTrigger asChild>
         <Button variant="outline" size="icon">
-          <PersonIcon className="h-4 w-4" />
+          <LayersIcon className="h-4 w-4" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="absolute z-50 w-[35vw] min-w-fit">
